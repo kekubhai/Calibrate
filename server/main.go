161e4ff1633e99966c753c65c0084a9b32cdbeaf
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-12 23:53:38 +0530

    add websocket upgrader and modify handler function signature

--- server/main.go
+++ server/main.go
@@ -1,7 +1,17 @@
 package main
+
 import (
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader=websocket.Upgrader {
+	CheckOrigin: fuck(r *http.Request) bool {
+		return true
+
+	},
+}
 func handler(w httpResponseWriter, r *httpRequest) {
 
 }
