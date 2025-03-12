14ad822250572c3c2bbe947b9b1eee15d06d5124
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-12 23:49:56 +0530

    add websocket dependency and implement main server function

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module example.com/server
 
 go 1.23.5
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- server/main.go
+++ server/main.go
@@ -5,3 +5,8 @@ import (
 func handler(w httpResponseWriter, r *httpRequest) {
 
 }
+func main (){
+	http.HandleFunc("w", handler){
+		http.ListenServer(":8080", nil )
+	}
+}
