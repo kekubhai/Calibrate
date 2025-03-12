edee9499cd33cf5db64f0ec82ba463684ca34bc2
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-13 00:00:58 +0530

    fix: improve websocket upgrade handling and add error logging

--- server/main.go
+++ server/main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-var upgrader=websocket.Upgrader {
-	CheckOrigin: fuck(r *http.Request) bool {
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
 		return true
-
 	},
 }
-func handler(w httpResponseWriter, r *httpRequest) {
-conn , err:=upgrader.Upgrade(w , r , nil)
-if err!=nil {
-	return (err)
 
-}
-defer conn.Close()
-}
-func main (){
-	http.HandleFunc("w", handler){
-		http.ListenServer(":8080", nil )
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+for {
+	messageType , message , err:= conn.ReadMessage()
+	if err!=nil {
+    fmt.Print(err)
+	return
+	}
+}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
