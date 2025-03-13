747ee331e626b213c7342c34e2de772fad534408
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-13 09:07:59 +0530

    fix: enhance message handling in websocket connection and include timestamp in responses

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -19,13 +20,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-for {
-	messageType , message , err:= conn.ReadMessage()
-	if err!=nil {
-    fmt.Print(err)
-	return
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		fmt.Printf("Recieved : %s\n", message)
+		res := fmt.Sprintf("%s, at: %s", string(message), time.Now().string)
+		if err := conn.WriteMessage(messageType, []byte(res)); err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
-}
 }
 
 func main() {
